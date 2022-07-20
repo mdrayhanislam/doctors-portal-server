55111c1d7f63e1308ec3772ff52134a38edbabc4
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-07-20 09:27:50 +0600

    mongobd connect

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 // const req = require('express/lib/request');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri =` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otxavnw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 app.get('/', (req, res) => {
   res.send('Hello From Doctor-portal')
 })
