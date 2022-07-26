0fd1d799583623bc2d44fa74929764e5ccb1f094
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-07-26 13:06:48 +0600

    API naming convention and simple Booking API

--- index.js
+++ index.js
@@ -19,12 +19,20 @@ async function run (){
 try{
   await client.connect();
   const serviceCollection = client.db("doctors-portal").collection("services");
+  const bookingCollection = client.db("doctors-portal").collection("bookings");
 
   app.get('/service', async(req, res) =>{
     const query = {};
     const cursor = serviceCollection.find(query);
     const services = await cursor.toArray();
     res.send(services);
+  })
+
+  app.post('/booking', async(req, res) =>{
+    const booking = req.body;
+    const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient}
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
   })
 }
 finally{
